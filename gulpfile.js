80589a92309413b1415a56d215a260d327fd2f50
Amr Khaled Hassan <63196889+3mr-5aled@users.noreply.github.com>
2023-01-31 11:39:16 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,12 @@ const cleanCSS = require("gulp-clean-css")
 const concat = require("gulp-concat")
 // Enabling you to compile your Pug templates into HTML
 const pug = require("gulp-pug")
+// js obfuscator
+// const javascriptObfuscator = require("gulp-javascript-obfuscator")
+// convert scss into css
+const sass = require("gulp-sass")(require("sass"))
+// js to babel
+const babel = require("gulp-babel")
 
 function pugtoHTML() {
   return (
@@ -23,16 +29,31 @@ function pugtoHTML() {
 
 function moveCss() {
   return gulp
-    .src(["src/styles/*.css"])
+    .src("src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
     .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(concat("all.css"))
     .pipe(gulp.dest("build"))
     .pipe(livereload())
+}
+
+function jsBabel() {
+  return gulp
+    .src("src/js/*js")
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
+    .pipe(concat("all.js"))
+    .pipe(gulp.dest("build"))
 }
 
 exports.default = function () {
   require("./server.js")
   livereload.listen()
 
-  gulp.watch(["src/**/*.pug"], parallel(pugtoHTML, moveCss))
+  gulp.watch(
+    ["src/**/*.pug", "src/sass/**/*.scss", "src/js/*.js"],
+    parallel(pugtoHTML, moveCss, jsBabel)
+  )
 }
